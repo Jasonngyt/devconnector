3e147b406a73451026e5e2c2834d67c98b1db220
jasonngyt <jasonngyt@gmail.com>
2020-06-27 21:44:12 +0800

    Post Reducer, Actions & Inital Component

---
+++ client/src/reducers/post.js
@@ -0,0 +1,35 @@
+import {
+    GET_POSTS,
+    POST_ERROR
+} from '../actions/types';
+
+const initialStatae = {
+    posts: [],
+    post: null,
+    loading: true,
+    error: {}
+}
+
+export default function (state = initialStatae, action) {
+    const {
+        type,
+        payload
+    } = action;
+
+    switch (type) {
+        case GET_POSTS:
+            return {
+                ...state,
+                posts: payload,
+                    loading: false
+            }
+            case POST_ERROR:
+                return {
+                    ...state,
+                    error: payload,
+                        loading: false
+                }
+                default:
+                    return state;
+    }
+}
