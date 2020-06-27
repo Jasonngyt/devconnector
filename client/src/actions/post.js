ee0c6092db31d0da01efda320fa09de51cc32faf
jasonngyt <jasonngyt@gmail.com>
2020-06-28 00:56:39 +0800

    Delete Post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -5,7 +5,8 @@ import {
 import {
     GET_POSTS,
     POST_ERROR,
-    UPDATE_LIKES
+    UPDATE_LIKES,
+    DELETE_POST,
 } from './types';
 
 
@@ -64,6 +65,28 @@ export const removeLike = id => async dispatch => {
                 likes: res.data
             }
         })
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status,
+            },
+        });
+    }
+}
+
+//Delete post
+export const deletePost = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/posts/${id}`);
+
+        dispatch({
+            type: DELETE_POST,
+            payload: id
+        })
+
+        dispatch(setAlert('Post Removed', 'success'))
     } catch (err) {
         dispatch({
             type: POST_ERROR,
